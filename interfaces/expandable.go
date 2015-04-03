9909de930e8540867a2a7b7057f41d8397f62859
Claudiu Codoban <claudiu.codoban@unifiedpost.com>
2015-04-03 16:02:28 +0300

    Added some comments

--- interfaces/expandable.go
+++ interfaces/expandable.go
@@ -1,5 +1,11 @@
 package interfaces
 
+// Interface used to expand JSON entites that contain different relations.
+// In short, entity IDs are transformed in those entities in full
+// inside the json objects that implement this interface.
+//
+// Each database model should have an expanded representation
+// that is used for receiving/returning data from requests
 type Expandable interface {
     Expand(obj Object) error
     Collapse() (*Object, error)
--- servers/rpc_server.go
+++ servers/rpc_server.go
@@ -9,6 +9,11 @@ import (
     "net/rpc"
 )
 
+// Starts an RPC (remote process call) server that is used internally
+// by the HTTP server.
+// This is used for easier mapping between routes and functions that
+// are used by the roots in order to respond to the requests.
+// Using the RPC server this way results in great horizontal scalability
 func StartRPCServer() {
     rpc.Register(new(api.Api))
     rpc.HandleHTTP()
