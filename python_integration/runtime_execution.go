ca304e6f9b19073c9a162471280076488fa9999a
Claudiu Codoban <codobanclaudiu@gmail.com>
2015-04-05 05:50:14 +0300

    Added module for executing a python script by injecting the code and parameters

---
+++ python_integration/runtime_execution.go
@@ -0,0 +1,22 @@
+package python_integration
+
+import (
+    "net/url"
+    "os/exec"
+    "strings"
+)
+
+const scriptFile = "script.py"
+
+func ExecuteCommand(codeBody string, parameters url.Values) ([]byte, error) {
+    head := "script.py"
+    params := make([]string, len(parameters))
+
+    params = append(params, codeBody)
+
+    for _, p := range parameters {
+        params = append(params, p)
+    }
+
+    return exec.Command(head, params...).Output()
+}
